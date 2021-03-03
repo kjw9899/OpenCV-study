c31eee47a5cbf1d2988e3ea985d5094e1d36c834
Kim Jae Won <wodnjs284@naver.com>
2021-03-03 22:36:33 +0900

    OpenCV - 5. 창 관리 및 이벤트 처리

---
+++ OpenCV - 5. 창 관리 및 이벤트 처리.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### 창관리
+
+# In[4]:
+
+
+# 창 관리 (win.py)
+
+import cv2
+
+file_path = 'C:/cdd/232.jpg'
+img = cv2.imread(file_path)                            # 이미지를 기본 값으로 읽기
+img_gray = cv2.imread(file_path, cv2.IMREAD_GRAYSCALE) # 이미지를 그레이 스케일로 읽기
+
+cv2.namedWindow('origin')                               # origin 이름으로 창 생성
+cv2.namedWindow('gray', cv2.WINDOW_NORMAL)              # gray 이름으로 창 생성
+cv2.imshow('origin', img)                               # origin 창에 이미지 표시
+cv2.imshow('gray', img_gray)                            # gray 창에 이미지 표시
+
+cv2.moveWindow('origin', 0, 0)                          # 창 위치 변경
+cv2.moveWindow('gray', 100, 100)                        # 창 위치 변경
+
+cv2.waitKey(0)                                          # 아무키나 누르면
+cv2.resizeWindow('origin', 200, 200)                    # 창 크기 변경 (변경 안됨)
+cv2.resizeWindow('gray', 100, 100)                      # 창 크기 변경 (변경 됨))
+
+cv2.waitKey(0)                                          # 아무키나 누르면
+cv2.destroyWindow("gray")                               # gray 창 닫기
+
+cv2.waitKey(0)                                          # 아무키나 누르면
+cv2.destroyAllWindows()  
+
+
+# ### 키보드 이벤트 처리
+
+# In[ ]:
+
+
+# 키 이벤트 처리 (event_key.py)
+
+import cv2
+
+img_file = "../img/yeosu.jpg" 
+img = cv2.imread(img_file) 
+title = 'IMG'                   # 창 이름 
+x, y = 100, 100                 # 최초 좌표
+
+while True:
+    cv2.imshow(title, img)
+    cv2.moveWindow(title, x, y)
+    key = cv2.waitKey(0) & 0xFF # 키보드 입력을 무한 대기, 8비트 마스크처리
+    print(key, chr(key))        # 키보드 입력 값,  문자 값 출력
+    if key == ord('h'):         # 'h' 키 이면 좌로 이동
+        x -= 10
+    elif key == ord('j'):       # 'j' 키 이면 아래로 이동
+        y += 10
+    elif key == ord('k'):       # 'k' 키 이면 위로 이동
+        y -= 10
+    elif key == ord('l'):       # 'l' 키 이면 오른쪽으로 이동
+        x += 10
+    elif key == ord('q') or key == 27: # 'q' 이거나 'esc' 이면 종료
+        break
+        cv2.destroyAllWindows()
+    cv2.moveWindow(title, x, y )   # 새로운 좌표로 창 이동
+        
+
+
+# ### 마우스 이벤트 처리
+
+# In[ ]:
+
+
+# 마우스 이벤트로 원 그리기 (event_mouse_circle.py)
+
+import cv2
+
+title = 'mouse event'                   # 창 제목
+img = cv2.imread('../img/blank_500.jpg') # 백색 이미지 읽기
+cv2.imshow(title, img)                  # 백색 이미지 표시
+
+def onMouse(event, x, y, flags, param): # 아무스 콜백 함수 구현 ---①
+    print(event, x, y, )                # 파라미터 출력
+    if event == cv2.EVENT_LBUTTONDOWN:  # 왼쪽 버튼 누름인 경우 ---②
+        cv2.circle(img, (x,y), 30, (0,0,0), -1) # 지름 30 크기의 검은색 원을 해당 좌표에 그림
+        cv2.imshow(title, img)          # 그려진 이미지를 다시 표시 ---③
+
+cv2.setMouseCallback(title, onMouse)    # 마우스 콜백 함수를 GUI 윈도우에 등록 ---④
+
+while True:
+    if cv2.waitKey(0) & 0xFF == 27:     # esc로 종료
+        break
+cv2.destroyAllWindows()
+
+
+# In[ ]:
+
+
+# flags를 활용하여 원 그리기 (event_mouse_circle_flag.py)
+
+import cv2
+
+title = 'mouse event'                   # 창 제목
+img = cv2.imread('../img/blank_500.jpg') # 백색 이미지 읽기
+cv2.imshow(title, img)                  # 백색 이미지 표시
+
+colors = {'black':(0,0,0),
+         'red' : (0,0,255),
+         'blue':(255,0,0),
+         'green': (0,255,0) } # 색상 미리 정의
+
+def onMouse(event, x, y, flags, param): # 아무스 콜백 함수 구현 ---①
+    print(event, x, y, flags)                # 파라미터 출력
+    color = colors['black']
+    if event == cv2.EVENT_LBUTTONDOWN:  # 왼쪽 버튼 누름인 경우 ---②
+        # 컨트롤키와 쉬프트 키를 모두 누른 경우
+        if flags & cv2.EVENT_FLAG_CTRLKEY and flags & cv2.EVENT_FLAG_SHIFTKEY : 
+            color = colors['green']
+        elif flags & cv2.EVENT_FLAG_SHIFTKEY : # 쉬프트 키를 누른 경우
+            color = colors['blue']
+        elif flags & cv2.EVENT_FLAG_CTRLKEY : # 컨트롤 키를 누른 경우
+            color = colors['red']
+        # 지름 30 크기의 검은색 원을 해당 좌표에 그림
+        cv2.circle(img, (x,y), 30, color, -1) 
+        cv2.imshow(title, img)          # 그려진 이미지를 다시 표시 ---③
+
+cv2.setMouseCallback(title, onMouse)    # 마우스 콜백 함수를 GUI 윈도우에 등록 ---④
+
+while True:
+    if cv2.waitKey(0) & 0xFF == 27:     # esc로 종료
+        break
+cv2.destroyAllWindows()
+
